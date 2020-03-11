61c9cf0de364b340ee5b05890bbff5e74caeaf6f
Scott Garner <scott@j38.net>
2020-03-11 12:51:44 -0400

    Class to fire events when triggers are hit.

---
+++ Unity/CarePackage/Assets/CarePackageDepot/Scripts/AutoTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class AutoTriggerEvent : UnityEvent<Collider> { }
+
+public class AutoTrigger : MonoBehaviour
+{
+    public AutoTriggerEvent OnAutoTriggerEnter;
+    public AutoTriggerEvent OnAutoTriggerExit;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        OnAutoTriggerEnter?.Invoke(collider);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        OnAutoTriggerExit?.Invoke(collider);
+    }
+}
