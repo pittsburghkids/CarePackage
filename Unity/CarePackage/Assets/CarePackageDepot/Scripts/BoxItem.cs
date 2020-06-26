9a889138586c508272f73d0bb5fce03a1f4a2322
Scott Garner <scott@j38.net>
2020-06-26 13:22:46 -0400

    New script for items that fly out of the box.

---
+++ Unity/CarePackage/Assets/CarePackageDepot/Scripts/BoxItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxItem : MonoBehaviour
+{
+    public float lifeTime = 1f;
+
+    private float startTime;
+
+    private Vector3 velocity;
+    private float gravity = 4;
+    private float drag = .2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = ((Vector3.up * Random.Range(1.5f, 2.5f)) + (Vector3.right * Random.Range(-1f, 1f)));
+        startTime = Time.time;
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Time.time - startTime) / lifeTime;
+        transform.localScale = Vector3.one * (1.0f - t);
+
+        velocity -= (Vector3.up * gravity) * Time.deltaTime;
+        velocity -= velocity * drag * Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+    }
+}
